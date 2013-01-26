e74f29a170add790cadc5b99fcece126b364087f
Rochais <aricdestroy@gmail.com>
2013-01-26 16:18:56 +0100

    initial commit: project creation

---
+++ src/com/barestodo/android/MainActivity.java
@@ -0,0 +1,41 @@
+package com.barestodo.android;
+
+import com.barestodo.android.service.ListPlace;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          android.R.layout.simple_list_item_1, android.R.id.text1, ListPlace.getListPlace());
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter); 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
---
+++ src/com/barestodo/android/service/ListPlace.java
@@ -0,0 +1,18 @@
+package com.barestodo.android.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPlace {
+
+	public static List<String> getListPlace(){
+		List<String> placeList = new ArrayList<String>();
+			
+		placeList.add("Troll");
+		placeList.add("Cantada");
+		placeList.add("Renaissance");
+		placeList.add("Quebecois");
+		return (placeList);
+	}
+	
+}
