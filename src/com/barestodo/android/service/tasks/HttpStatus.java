e75b2a4b2e8e3a2d43648e925ef7d07647744f1e
Fabien Maury <fab.maury@gmail.com>
2013-04-07 21:54:00 +0200

    improve error management

--- src/com/barestodo/android/service/tasks/HttpStatus.java
+++ src/com/barestodo/android/service/tasks/HttpStatus.java
@@ -1,11 +1,11 @@
 package com.barestodo.android.service.tasks;
 
-import android.content.res.Resources;
 import com.barestodo.android.R;
+import com.barestodo.android.app.MyApplication;
 
 
 public enum HttpStatus {
-    OK(0),NOT_FOUND(R.string.not_found),BAD_REQUEST(R.string.invalid_request),FORBIDEN(R.string.authentication_problem),SERVER_ERROR(R.string.service_unavailable),CONNECTION_LOST(R.string.connection_problem), CLIENT_ERROR(R.string.not_found);
+    OK(0),NOT_FOUND(R.string.not_found),BAD_REQUEST(R.string.invalid_request),FORBIDEN(R.string.authentication_problem),SERVER_ERROR(R.string.service_unavailable),CONNECTION_LOST(R.string.connection_problem), CLIENT_ERROR(R.string.application_error);
 
 
     private int labelId;
@@ -16,6 +16,6 @@ public enum HttpStatus {
 
 
     public String getErrorMessage() {
-        return Resources.getSystem().getString(labelId);
+        return MyApplication.getAppContext().getString(labelId);
     }
 }
